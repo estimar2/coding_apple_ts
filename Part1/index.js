ffe224df28c818915f016f1a8adde0eb5bf4e30b
estimar2 <songsm5504@gmail.com>
2024-06-13 18:19:08 +0900

    [24.06.13] 타입스크립트 기본 타입 정리(primitive types)

---
+++ Part1/index.js
@@ -0,0 +1,7 @@
+var userName = "codding_apple";
+var userAge = 50;
+var userLocation = "Korea";
+var music = {
+    singer: "aespa",
+    song: "armageddon",
+};
---
+++ Part1/index.ts
@@ -0,0 +1,10 @@
+let userName: string = "codding_apple";
+let userAge: number = 50;
+let userLocation: string = "Korea";
+
+let music: {
+  [key: string]: string;
+} = {
+  singer: "aespa",
+  song: "armageddon",
+};
